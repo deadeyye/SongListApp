d58a06c7433f53ef59eecb24268e1bff6bb3cdf4
Deadeyye <deadeyye@gmail.com>
2020-10-31 12:31:19 +0100

    generic adapter

---
+++ app/src/main/java/com/pawelsmagala/songlistapp/SongListActivity/SongAdapter.kt
@@ -0,0 +1,23 @@
+package com.pawelsmagala.songlistapp.songListActivity
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.pawelsmagala.domain.song.Song
+import com.pawelsmagala.songlistapp.GenericViewBindingAdapter
+import com.pawelsmagala.songlistapp.databinding.SongListItemBinding
+
+class SongAdapter() : GenericViewBindingAdapter<SongListItemBinding, Song>() {
+    override fun generateViewWithBinding(parent: ViewGroup): SongListItemBinding {
+        return SongListItemBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+    }
+
+    override fun fillViewWithData(bindingView: SongListItemBinding, item: Song) {
+        bindingView.txtFirstLine.text = item.songName
+        bindingView.txtSecondLine.text = item.getDescription()
+    }
+
+
+}
---
+++ app/src/main/java/com/pawelsmagala/songlistapp/SongListActivity/SongListActivity.kt
@@ -0,0 +1,47 @@
+package com.pawelsmagala.songlistapp.songListActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.pawelsmagala.songlistapp.SongListActivity.SongListViewModel
+import com.pawelsmagala.songlistapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SongListActivity : AppCompatActivity() {
+
+    private val viewModel: SongListViewModel by viewModels()
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    private val songAdapter = SongAdapter()
+
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupRecyclerView()
+
+        viewModel.songLiveData.observe(this, {
+                songList -> songAdapter.setItemList(songList)
+        })
+
+    }
+
+    private fun setupRecyclerView() {
+        binding.songListRecycler.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(this@SongListActivity)
+            adapter = songAdapter
+        }
+    }
+}
